ea51004600d51e50c5819f0fa867ea3aa28e419b
Caio Sym <caio.sym@climate.com>
2018-07-24 19:27:36 -0300

    Projects Screen style

---
+++ app/javascript/bundles/App/containers/NavBar/NavBar.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar as BSNavBar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+import './NavBar.css'
+
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <BSNavBar className="NavBarContainer" expand="md">
+        <NavbarBrand href="/">Symple</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/caiosym">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </BSNavBar>
+    );
+  }
+}
